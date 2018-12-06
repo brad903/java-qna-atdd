0b457bfa91b128b8805716f482cb6fb3483baaa9
Brad Lee <brad903@naver.com>
2018-12-06 15:12:47 +0900

    ATDD 수업 실습

--- src/main/java/codesquad/controller/LoginController.java
+++ src/main/java/codesquad/controller/LoginController.java
@@ -1,4 +1,30 @@
 package codesquad.controller;
 
+import codesquad.UnAuthenticationException;
+import codesquad.domain.User;
+import codesquad.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login/form")
+    public String loginForm() {
+        return "user/form";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password) {
+        try {
+            User user = userService.login(userId, password);
+            return "redirect:/";
+        } catch (Exception e) {
+            return "user/login_failed";
+        }
+    }
 }
--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -1,4 +1,58 @@
 package codesquad.web;
 
-public class LoginAcceptanceTest {
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import support.test.AcceptanceTest;
+
+import java.util.Arrays;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class LoginAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = getLogger(LoginAcceptanceTest.class);
+
+    @Test
+    public void loginForm() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/login/form", String.class);  // get방식으로 요청을 보냄
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);  // softly를 쓰면 따로 import 필요없음
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void login() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("userId", "javajigi");
+        params.add("password", "test");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = template().postForEntity("/login", request, String.class);
+
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);  // Controller에서 redirect시 302 상태메시지를 보냄
+        softly.assertThat(response.getHeaders().getLocation().getPath()).isEqualTo("/");
+    }
+
+    @Test
+    public void login_failed() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("userId", "javajigi");
+        params.add("password", "test2");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = template().postForEntity("/login", request, String.class);
+
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);  // Controller에서 redirect시 302 상태메시지를 보냄
+        softly.assertThat(response.getBody().contains("아이디 또는 비밀번호가 틀립니다")).isTrue();
+    }
+
 }
