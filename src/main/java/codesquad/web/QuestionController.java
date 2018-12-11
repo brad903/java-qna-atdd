1a41cbc664a46808033476cc5858ab147bddfea6
Brad Lee <brad903@naver.com>
2018-12-11 16:07:19 +0900

    질문 생성/상세보기, home 질문목록 구현

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,9 +1,50 @@
 package codesquad.web;
 
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.hibernate.annotations.GeneratorType;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityNotFoundException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
+    private static final Logger log = getLogger(QuestionController.class);
+
+    @Resource(name = "qnaService")
+    private QnaService qnaService;
+
+    @GetMapping("/form")
+    public String form(@LoginUser User loginUser) {
+        return "qna/form";
+    }
+
+    @PostMapping("")
+    public String create(@LoginUser User loginUser, Question question, Model model) {
+        question = qnaService.create(loginUser, question);
+        model.addAttribute("question", question);
+        return "qna/show";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Question question = qnaService.findById(id)
+                            .orElseThrow(EntityNotFoundException::new);
+        model.addAttribute("question", question);
+        return "qna/show";
+    }
+
+
 }
--- src/test/java/codesquad/web/QuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -1,6 +1,73 @@
 package codesquad.web;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
+import support.test.HtmlFormDataBuilder;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class QuestionAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = getLogger(QuestionAcceptanceTest.class);
+    HttpEntity<MultiValueMap<String, Object>> request;
+
+    @Before
+    public void setUp() throws Exception {
+        request = HtmlFormDataBuilder.urlEncodedForm()
+                .addParameter("title", "제목 테스트")
+                .addParameter("contents", "내용 테스트 - 코드스쿼드 qna-atdd step2 진행중입니다")
+                .build();
+    }
+
+    @Test
+    public void form_no_login() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void form_login() throws Exception {
+        ResponseEntity<String> response = basicAuthTemplate(defaultUser()).getForEntity("/questions/form", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void create_no_login() throws Exception {
+        ResponseEntity<String> response = template().postForEntity("/questions", request, String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void create_login() throws Exception {
+        ResponseEntity<String> response = basicAuthTemplate(defaultUser()).postForEntity("/questions", request, String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        softly.assertThat(response.getBody()).contains(defaultUser().getName());
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void show_inserted_question() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/questions/1", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void show_not_inserted_question() {
+        ResponseEntity<String> response = template().getForEntity("/questions/100", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void home() {
+        ResponseEntity<String> response = template().getForEntity("/", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.debug("body : {}", response.getBody());
+    }
 }
