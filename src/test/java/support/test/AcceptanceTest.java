a37dc29a436d647f5ecddddfe37a56b0d79ee964
Brad Lee <brad903@naver.com>
2018-12-16 20:25:11 +0900

    ApiUserAcceptanceTest의 중복코드 제거 리팩토링 - AcceptanceTest에 메서드 추가

--- src/test/java/support/test/AcceptanceTest.java
+++ src/test/java/support/test/AcceptanceTest.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)  // 서버를 알아서 가동시켜줌
@@ -38,5 +41,21 @@ public abstract class AcceptanceTest extends BaseTest {
 
     protected User findByUserId(String userId) {
         return userRepository.findByUserId(userId).get();
+    }
+
+    protected String createResource(String path, Object bodyPayload) {
+        ResponseEntity<Void> response = template().postForEntity(path, bodyPayload, Void.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        return response.getHeaders().getLocation().getPath();
+    }
+
+    protected <T> ResponseEntity<T> getForEntity(String location, Class<T> responseType,
+                                              User loginUser) throws RestClientException {
+        return basicAuthTemplate(loginUser).getForEntity(location, responseType, loginUser);
+    }
+
+    protected <T> T getForObject(String location, Class<T> responseType, User loginUser)
+            throws RestClientException {
+        return basicAuthTemplate(loginUser).getForObject(location, responseType, loginUser);
     }
 }
